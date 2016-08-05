264a3ed1c68bf7667e70ab831c4934797de810ed
ddrccw <ddrccw@gmail.com>
2016-08-05 18:06:24 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+export PATH=/opt/local/bin/:/opt/local/sbin:$PATH:/usr/local/bin:
+
+INFO_CLR="\033[01;33m"
+RESULT_CLR="\033[01;32m"
+RESET_CLR="\033[0m"
+ERR_CLR="\033[01;31m"
+
+TMUX_PATH=`which tmux`
+
+if [[ ! -f ${TMUX_PATH} || -z ${TMUX_PATH} ]]; then
+	echo "WARNING: You need to install tmux first, you can use brew to simplify process:"
+	echo "brew install tmux"
+	exit
+fi
+
+TEMP_INSTALL_PATH=~/
+CFG_PATH="$TEMP_INSTALL_PATH"/mac-config
+VIM_PATH="$CFG_PATH"/vim
+
+echo "$INFO_CLR-------------------- Downloading Mac-config --------------------$INFO_CLR"
+
+cd "$TEMP_INSTALL_PATH"
+rm -rf "$TEMP_INSTALL_PATH"/mac-config
+
+git clone git@github.com:ddrccw/mac-config.git
+
+echo "$RESULT_CLR-------------------- Download Mac-config --------------------$RESULT_CLR"
+
+echo "$INFO_CLR-------------------- Downloading Submodule --------------------$INFO_CLR"
+
+cd "$CFG_PATH"
+git submodule init
+git submodule update 
+
+
+cd "$VIM_PATH"
+git submodule init
+git submodule update
+
+echo "$RESULT_CLR-------------------- Download Submodule --------------------$RESULT_CLR"
+
+echo "$INFO_CLR-------------------- Making Symbolic Links --------------------$INFO_CLR"
+
+#vim
+ln -s ${VIM_PATH}/.vimrc ${HOME}/.vimrc
+ln -s ${VIM_PATH}/.vim ${HOME}/.vim
+
+#tmux
+ln -s ${CFG_PATH}/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${CFG_PATH}/.tmux ${HOME}/.tmux
+
+#others
+ln -s ${CFG_PATH}/.gitconfig ${HOME}/.gitconfig
+ln -s ${CFG_PATH}/.lldbinit ${HOME}/.lldbinit
+
+echo "$RESULT_CLR-------------------- Make Symbolic Links  --------------------$RESULT_CLR"
+
+echo "$INFO_CLR-------------------- Installing Tmux Plugins --------------------$INFO_CLR"
+
+${HOME}/.tmux/plugins/tpm/bin/install_plugins
+
+echo "$RESULT_CLR-------------------- Install Symbolic Links  --------------------$RESULT_CLR"
+
+echo "$INFO_CLR-------------------- Installing Vim Plugins --------------------$INFO_CLR"
+
+vim +PluginInstall +qall
+
+echo "$RESULT_CLR-------------------- Installing Vim Plugins  --------------------$RESULT_CLR"
+
